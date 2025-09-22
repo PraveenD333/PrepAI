64796574bf1546b28312c1ab278132e73caa7515
Praveen <praveendhane97@gmail.com>
2025-09-23 00:47:42 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ import { generateConceptExplanation, generateInterviewQuestions } from './Contro
 const app=express();
 
 app.use(cors({
-    origin:"praveen-prep-ai-ivory.vercel.app",
+    origin:"https://praveen-prep-ai-ivory.vercel.app",
     methods:["GET","POST","PUT","DELETE"],
     allowedHeaders: ["Content-type","Authorization"],
 }));
